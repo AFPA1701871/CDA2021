cdc61eaf31a8743ed07f6b4503ff6756dded7dea
AFPA1701871 <martine.poix@afpa.fr>
2021-10-05 12:24:44 +0200

    05/10/2021 12:24:43,52

---
+++ 04 - Sécurité/02 - XSS Cross Side/PHP/Reponse.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<HTML>
+<head>
+    <meta charset="UTF-8" />
+    <title>structureSite</title>
+    <link rel="stylesheet" href="CSS/style.css" />
+</head>
+<body>
+    <div class="colonne">
+        <div><h1>Formulaire de recherche</h1></div>
+        
+        <div><h1>Réponse</h1></div>
+    </div>
+  
+   <?php if (isset($_POST["nom"]))
+    {
+        echo '<div class="resultat"> Le résultat de votre recherche pour : '.$_POST["nom"].'</div>';
+      // pour pallier, utiliser htmlspecialchars
+      /* var_dump(htmlspecialchars($_POST["nom"]));
+        echo '<div class="resultat"> Le résultat de votre recherche pour : '.htmlspecialchars($_POST["nom"]).'</div>';
+        */
+    }
+    // ecrire le php classique de recherche
+    ?>
+    <a href="?page=Formulaire" ><button>Retour</button></a>
+</body>
+
+</html>
