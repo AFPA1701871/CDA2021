51ed0141fa43ed4909ec1d3858025b03e15261d6
AFPA1701871 <martine.poix@afpa.fr>
2021-10-25 16:58:57 +0200

    doc + exemples

---
+++ 2 - Composants d'interface/2 - Interface Desktop/1 - Développer en C#/3 - Solutions/Decouverte/Decouverte/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Decouverte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(a);
+
+            Console.Read();
+        }
+    }
+}
