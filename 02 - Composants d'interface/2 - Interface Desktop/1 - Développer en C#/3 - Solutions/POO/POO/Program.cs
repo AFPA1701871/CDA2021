aa617d083474c519923899ff59da169c39b078c9
afpa1701871 <martine.poix@afpa.fr>
2021-11-04 17:13:13 +0100

    04/11/2021 17:13:10,43

---
+++ 02 - Composants d'interface/2 - Interface Desktop/1 - Développer en C#/3 - Solutions/POO/POO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personnes p = new Personnes();
+            Personnes toto = new Personnes("DUPOND", "Toto", 20, "Ici");
+            // p.nom="TRUC" si attribut public
+            p.SetNom( "TRUC"); // si attribut privé
+
+            /* si public */
+            //Console.WriteLine("nom de p : " + p.nom);
+            //Console.WriteLine("info de toto : " + toto.nom + " " + toto.prenom +" " + toto);
+
+            /* si privé */
+            Console.WriteLine("nom de p : " + p.GetNom());
+            Console.WriteLine(p.ToString());
+
+            Console.WriteLine("info de toto : " + toto.GetNom() + " " + toto.GetPrenom() +" " + toto.ToString());
+
+
+            Personnes p1 = new Personnes("MACHIN", "Bidule");
+            Console.WriteLine("p1 " + p1.ToString());
+        }
+    }
+}
