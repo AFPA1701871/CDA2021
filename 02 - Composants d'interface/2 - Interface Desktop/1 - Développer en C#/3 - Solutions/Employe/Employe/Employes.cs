f282f0c503919527b129a72f005dccb0f15f1a4f
AFPA1701871 <martine.poix@afpa.fr>
2021-11-10 12:24:49 +0100

    10/11/2021 12:24:45,64

--- 02 - Composants d'interface/2 - Interface Desktop/1 - Développer en C#/3 - Solutions/Employe/Employe/Employes.cs
+++ 02 - Composants d'interface/2 - Interface Desktop/1 - Développer en C#/3 - Solutions/Employe/Employe/Employes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Employe.Program;
 
 namespace Employe
 {
@@ -14,8 +15,10 @@ namespace Employe
         public string Fonction { get; set; }
         public double Salaire { get; set; }
         public string Service { get; set; }
+        public Agences Agence { get; set; }
+        public List<Enfants> ListeEnfants { get; set; } = new List<Enfants>();
 
-        public Employes(string nom, string prenom, DateTime dateEmbauche, string fonction, double salaire, string service)
+        public Employes(string nom, string prenom, DateTime dateEmbauche, string fonction, double salaire, string service, Agences agence, List<Enfants> listeEnfants)
         {
             Nom = nom;
             Prenom = prenom;
@@ -23,6 +26,8 @@ namespace Employe
             Fonction = fonction;
             Salaire = salaire;
             Service = service;
+            Agence = agence;
+            ListeEnfants = listeEnfants;
         }
 
         public int NbAnneesAnciennete()
@@ -43,7 +48,7 @@ namespace Employe
         }
         public double Prime()
         {
-            return Math.Round(this.PrimeSalaire()+this.PrimeAnciennete(),2); // on arrondi 2 chiffres après la virgule
+            return Math.Round(this.PrimeSalaire() + this.PrimeAnciennete(), 2); // on arrondi 2 chiffres après la virgule
         }
         /// <summary>
         /// Méthode théorique : en réalité la méthode serait déplacer en dehors de la classe Employes
@@ -51,7 +56,7 @@ namespace Employe
         public void OrdreDeTransfert()
         {
             DateTime dateVersement = new DateTime(DateTime.Now.Year, 11, 30); // DateTime.Now.Year donne l'année du jour en cours
-            if (DateTime.Now==dateVersement)  // peut être modifié par une comparaison de mois et jour uniquement
+            if (DateTime.Now == dateVersement)  // peut être modifié par une comparaison de mois et jour uniquement
             {
                 Console.WriteLine("Ordre de transfert de " + this.Prime() + "pour la salarié " + this.Nom);
             }
@@ -62,13 +67,16 @@ namespace Employe
             if (a.Nom.CompareTo(b.Nom) > 0)
             {
                 return 1;
-            }else if(a.Nom.CompareTo(b.Nom) < 0)
+            }
+            else if (a.Nom.CompareTo(b.Nom) < 0)
             {
                 return -1;
-            }else if(a.Prenom.CompareTo(b.Prenom) > 0) // les noms sont égaux, on compare les prenoms
+            }
+            else if (a.Prenom.CompareTo(b.Prenom) > 0) // les noms sont égaux, on compare les prenoms
             {
                 return 1;
-            }else if (a.Prenom.CompareTo(b.Prenom) < 0)
+            }
+            else if (a.Prenom.CompareTo(b.Prenom) < 0)
             {
                 return -1;
             }
@@ -95,9 +103,78 @@ namespace Employe
             return this.Salaire + this.Prime();
         }
 
+        public string ChequeVac()
+        {
+            if (this.NbAnneesAnciennete() >= 1)
+            {
+                return "L'employe bénéficie de chèques vacances";
+            }
+            return "L'employe ne bénéficie pas de chèques vacances";
+
+        }
+
+        public void AjouterEnfant(Enfants enfant)
+        {
+            this.ListeEnfants.Add(enfant);
+        }
+
+        public string ChequeNoel()
+        {
+            int[] tab = new int[3] { 0, 0, 0 }; // on stocke le nombre de chèque de chaque montant
+            string reponse = "";
+            foreach (var enfant in ListeEnfants)
+            {
+                switch (enfant.ChequeNoel())
+                {
+                    case 20: tab[0]++; break;
+                    case 30: tab[1]++; break;
+                    case 50: tab[2]++; break;
+                    default: break;
+                }
+
+            }
+            if (tab.Sum() == 0)  // tab.Sum fait la somme de toutes les cases du tableau
+            {
+                return "Pas de droit aux chèques Noel";
+            }
+            if (tab[0] > 0) reponse += tab[0] + " chèque de 20\n";
+            if (tab[1] > 0) reponse += tab[1] + " chèque de 30\n";
+            if (tab[2] > 0) reponse += tab[2] + " chèque de 50\n";
+            return reponse;
+        }
+
+        public string ChequeNoel2()
+        {
+            int[] tab = new int[3] { 0, 0, 0 }; // on stocke le nombre de chèque de chaque montant
+            string reponse = "";
+
+            // parcours des enfants pour connaitre les valeurs des cheques
+            foreach (var enfant in ListeEnfants)
+            {
+                tab[(int)enfant.ChequeNoel2()]++;   // en fonction de la réponse de chequeNoel enfant, on incremente la case du tableau correspondante
+                // enfant.ChequeNoel2() renvoi par exemple C20, caster en int donne 0 (car le 1er de l'enumeration)
+            }
+
+
+            if (tab.Sum() == 0)  // tab.Sum fait la somme de toutes les cases du tableau
+            {
+                return "Pas de droit aux chèques Noel";
+            }
+            // comme la partie vrai contient un return, le reste ne s'execute que s'il a droit aux cheques Noel
+            for (var i = 0; i < tab.Length; i++)
+            { // on parcours le tableau pour connaitre le nb de cheque à distribuer
+                if (tab[i] > 0) reponse += tab[i] + " chèque de " + (Enum.GetName(typeof(ValeurChequeNoel), i)).Substring(1) + "\n";
+
+                // pour recuperer la valeur du cheque on cherche à obtenir C20 et on enleve le C
+                // Enum.GetName(typeof(ValeurChequeNoel), i) permet de récuperer le libellé associé à la valeur i
+                // .Substring(1) permet d'enlever le C
+            }
+
+            return reponse;
+        }
         public override string ToString()
         {
-            
+
             string reponse =
             "****_INFORMATION_SUR_L'EMPLOYES_****" +
             "\n|Nom                : " + this.Nom +
@@ -105,7 +182,13 @@ namespace Employe
             "\n|Date Embauche    : " + this.DateEmbauche.ToString("dd/MM/yyyy") +  // on formate la date avant l'affichage
             "\n|Fonction         : " + this.Fonction +
             "\n|Salaire          : " + this.Salaire +
-            "\n|Service          : " + this.Service;
+            "\n|Service          : " + this.Service +
+            "\n|" + this.ChequeVac() +
+            "\n|" + this.ChequeNoel() +
+            "\nsolution 2\n" +
+            "\n|" + this.ChequeNoel2() +
+
+            this.Agence;
             return reponse;
         }
     }
---
+++ 02 - Composants d'interface/2 - Interface Desktop/1 - Développer en C#/3 - Solutions/SpaceInvaders/SpaceInvaders/Space.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Space
+    {
+        public int NbLignes { get; set; }
+        public int NbColonnes { get; set; }
+        public char[,] Grille { get; set; }
+
+        public Space(int nbLignes, int nbColonnes)
+        {
+            NbLignes = nbLignes;
+            NbColonnes = nbColonnes;
+            this.CreerListe();
+        }
+
+        private void  CreerListe()
+        {
+            Grille = new char[this.NbLignes, this.NbColonnes];
+            for (int ligne =0; ligne< this.NbLignes;ligne++)
+            {
+                for (int colonne = 0; colonne < this.NbColonnes; colonne++)
+                {
+                    Grille[ligne, colonne] = ' ';
+                }
+            }
+        }
+
+        public void AfficherGrille()
+        {
+            for (int ligne = 0; ligne < this.NbLignes; ligne++)
+            {
+                for (int colonne = 0; colonne < this.NbColonnes; colonne++)
+                {
+                    Console.Write(Grille[ligne, colonne]);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public  void Afficher()
+        {
+            Console.WriteLine("nbLignes : " + NbLignes + "\n");
+            Console.WriteLine("nbColonnes : " + NbColonnes + "\n");
+            AfficherGrille();
+        }
+    }
+}
