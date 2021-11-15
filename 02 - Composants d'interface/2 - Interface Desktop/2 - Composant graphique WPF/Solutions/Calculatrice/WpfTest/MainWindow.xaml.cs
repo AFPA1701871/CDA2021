d88c407ce724be48150e38fc6251f79104f48a56
afpa1701871 <martine.poix@afpa.fr>
2021-11-15 17:16:47 +0100

    15/11/2021 17:16:43,47

---
+++ 02 - Composants d'interface/2 - Interface Desktop/2 - Composant graphique WPF/Solutions/Calculatrice/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnNumerique_Click(object sender, RoutedEventArgs e)
+        {
+            // sender est l'élément qui a déclenché l'événement
+            tbxResultat.Text += ((Button)sender).Content;
+        }
+
+        private void btnInit_Click(object sender, RoutedEventArgs e)
+        {
+            tbxResultat.Text = "";
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+           double w= Application.Current.MainWindow.Width;
+            double h = Application.Current.MainWindow.Height;
+            tbxResultat.Text = "w " + w + ", h " + h;
+            btn1.FontSize = h/20 ;
+        }
+    }
+}
