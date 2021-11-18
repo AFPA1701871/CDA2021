fddec0778fe5e8ae43e63e883a47a80c8a41e2ee
AFPA1701871 <martine.poix@afpa.fr>
2021-11-18 12:24:49 +0100

    18/11/2021 12:24:45,44

--- 02 - Composants d'interface/2 - Interface Desktop/2 - Graphique WPF/Solutions/GestionProduits/WpfApp2/obj/Debug/net5.0-windows/App.g.cs
+++ 02 - Composants d'interface/2 - Interface Desktop/2 - Graphique WPF/Solutions/GestionProduits/WpfApp2/obj/Debug/net5.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace GestionProduits {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace GestionProduits {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             GestionProduits.App app = new GestionProduits.App();
             app.InitializeComponent();
