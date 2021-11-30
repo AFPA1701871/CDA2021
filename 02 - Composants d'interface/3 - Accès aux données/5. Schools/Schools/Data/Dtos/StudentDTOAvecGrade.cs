9ffd3a449d43df407a92382f1cd52153c62a11cf
afpa1701871 <martine.poix@afpa.fr>
2021-11-30 08:38:01 +0100

    30/11/2021  8:38:00,92

---
+++ 02 - Composants d'interface/3 - Accès aux données/5. Schools/Schools/Data/Dtos/StudentDTOAvecGrade.cs
@@ -0,0 +1,20 @@
+﻿using Schools.Data.Models;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Schools.Data.Dtos
+{
+    public partial class StudentDTOAvecGrade
+    {
+        public StudentDTOAvecGrade()
+        {
+        }
+
+        public string Name { get; set; }
+        public int GradeId { get; set; }
+
+        public virtual GradeDTOOut Grade { get; set; }
+    }
+}
---
+++ 02 - Composants d'interface/3 - Accès aux données/5. Schools/Schools/Data/Dtos/StudentDTOOut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Schools.Data.Dtos
+{
+    public partial class StudentDTOOut
+    {
+        public StudentDTOOut()
+        {
+           
+        }
+
+        public string Name { get; set; }
+    }
+}
