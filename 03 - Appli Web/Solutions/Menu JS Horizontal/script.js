e87d5eaaf68643cd9559368fc3b21d0fbf8c0a9f
AFPA1701871 <martine.poix@afpa.fr>
2021-12-22 12:24:42 +0100

    22/12/2021 12:24:40,79

--- 03 - Appli Web/Solutions/Menu JS Horizontal/script.js
+++ 03 - Appli Web/Solutions/Menu JS Horizontal/script.js
@@ -6,6 +6,8 @@ var listeMenus = document.getElementsByClassName("menu");
 for (let i = 0; i < listeMenus.length; i++) {
     listeMenus[i].addEventListener("click", clickMenu);
     listeMenus[i].addEventListener("mouseover", clickMenu);
+    /* on ferme les menus quand on quitte un menu */
+    listeMenus[i].addEventListener("mouseleave", fermeMenu);
 }
 
 // on gère le survol et le clic des sous menus
@@ -15,23 +17,36 @@ listeSousMenus.forEach(element => {
     element.addEventListener("mouseover", clickSousMenu);
 });
 
+
+function fermeMenu(event) {
+    if (sousMenuItemActive != undefined) {
+        sousMenuItemActive.classList.remove("sousMenuItemActive");
+        sousMenuItemActive = undefined;
+    }
+    if (sousMenuOuvert != undefined && sousMenuOuvert != event.target.parentNode.parentNode) {
+        sousMenuOuvert.previousElementSibling.classList.remove("menuItemActive");
+        sousMenuOuvert.style.display = "none";
+        sousMenuOuvert = undefined;
+    }
+}
+
+
+
 /**
  * méthode qui gère le changement d'affichage des menus lors du survol ou du clic
  * @param {*} event  evenement qui a déclenché l'action
  */
 function clickMenu(event) {
     // si on vient d'un menuItem
-    if (event.target.classList.contains("menuItem"))
-    {
+    if (event.target.classList.contains("menuItem")) {
         var menuClique = event.target.parentNode;
         var sousMenu = menuClique.querySelector(".sousMenu");
         /* Changer la couleur du menu sélectionné */
         event.target.classList.add("menuItemActive")
+    } else { // on a cliqué sur un sousMenuItem
+        sousMenu = event.target.parentNode.parentNode;
     }
-    else{ // on a cliqué sur un sousMenuItem
-            sousMenu = event.target.parentNode.parentNode;
-    }
-    
+
     /* Fermer le sousMenu ouvert s'il est différent du menu à ouvrir*/
     if (sousMenuOuvert != undefined && sousMenuOuvert != sousMenu) {
         sousMenuOuvert.style.display = "none";
