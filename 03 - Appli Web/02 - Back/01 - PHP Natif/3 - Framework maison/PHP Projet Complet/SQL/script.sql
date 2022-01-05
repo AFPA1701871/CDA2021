51a7c9c8a60db8c65e581512d158a266ba90e67f
afpa1701871 <martine.poix@afpa.fr>
2022-01-05 16:03:08 +0100

    sql

--- 03 - Appli Web/02 - Back/01 - PHP Natif/3 - Framework maison/PHP Projet Complet/SQL/script.sql
+++ 03 - Appli Web/02 - Back/01 - PHP Natif/3 - Framework maison/PHP Projet Complet/SQL/script.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS baseproduits;
 
 CREATE DATABASE IF NOT EXISTS baseproduits DEFAULT CHARACTER
-SET latin1 COLLATE latin1_swedish_ci;
+SET utf8 COLLATE latin1_swedish_ci;
 
 USE baseproduits;
 
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS categories (
   idCategorie int (11) NOT NULL AUTO_INCREMENT,
   LibelleCategorie varchar (50) NOT NULL,
   PRIMARY KEY (idCategorie)
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 DROP TABLE IF EXISTS produits;
 
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS produits (
   idCategorie int (11) NOT NULL,
   PRIMARY KEY (idProduit),
   KEY FK_Produit_Categorie (idCategorie)
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 DROP TABLE IF EXISTS utilisateurs;
 
@@ -36,21 +36,94 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
   role int(11) NOT NULL COMMENT '2 Admin 1 User',
   pseudo varchar(50) NOT NULL,
   PRIMARY KEY (idUtilisateur)
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-ALTER TABLE produits
-ADD   CONSTRAINT FK_Produit_Categorie FOREIGN KEY (idCategorie) REFERENCES categories (idCategorie);
+DROP TABLE IF EXISTS `texte`;
+
+CREATE TABLE IF NOT EXISTS `texte` (
+  `idTexte` int(11) NOT NULL AUTO_INCREMENT,
+  `codeTexte` varchar(20) NOT NULL,
+  `codeLangue` varchar(2) NOT NULL,
+  `Texte` varchar(200) NOT NULL,
+  PRIMARY KEY (`idTexte`),
+  KEY `Code Texte` (`codeTexte`)
+) ENGINE = InnoDB AUTO_INCREMENT = 36 DEFAULT CHARSET = utf8;
+
+ALTER TABLE
+  produits
+ADD
+  CONSTRAINT FK_Produit_Categorie FOREIGN KEY (idCategorie) REFERENCES categories (idCategorie);
 
 INSERT INTO
   categories (idCategorie, LibelleCategorie)
-VALUES   (1, 'périssable'),(2, 'éternel');
+VALUES
+  (1, 'périssable'),
+(2, 'éternel');
 
-INSERT INTO   produits (idProduit, libelleProduit, prix, dateDePeremption, idCategorie  )
-VALUES (1, 'gomme', 2, '2020-11-30', 1);
+INSERT INTO
+  produits (
+    idProduit,
+    libelleProduit,
+    prix,
+    dateDePeremption,
+    idCategorie
+  )
+VALUES
+  (1, 'gomme', 2, '2020-11-30', 1);
 
-INSERT INTO   produits (idProduit, libelleProduit, prix, dateDePeremption, idCategorie  )
-VALUES(2, 'crayon', 1, '2020-11-30', 2);
+INSERT INTO
+  produits (
+    idProduit,
+    libelleProduit,
+    prix,
+    dateDePeremption,
+    idCategorie
+  )
+VALUES
+(2, 'crayon', 1, '2020-11-30', 2);
 
-INSERT INTO `utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `motDePasse`, `adresseMail`, `role`, `pseudo`) VALUES
-(7, 'ad', 'ad', '11d437a3e6695447bd1bf2331651049e', 'ad', 2, 'ad'),
-(8, 'u', 'u', '1d0a5a28d53430e7f2293a1f36682f23', 'u', 1, 'u');
+INSERT INTO
+  `utilisateurs` (
+    `idUtilisateur`,
+    `nom`,
+    `prenom`,
+    `motDePasse`,
+    `adresseMail`,
+    `role`,
+    `pseudo`
+  )
+VALUES
+  (
+    7,
+    'ad',
+    'ad',
+    '11d437a3e6695447bd1bf2331651049e',
+    'ad',
+    2,
+    'ad'
+  ),
+  (
+    8,
+    'u',
+    'u',
+    '1d0a5a28d53430e7f2293a1f36682f23',
+    'u',
+    1,
+    'u'
+  );
+
+INSERT INTO
+  `texte` (`idTexte`, `codeTexte`, `codeLangue`, `Texte`)
+VALUES
+  (1, 'Accueil', 'FR', 'Accueil'),
+  (2, 'titrePage', 'FR', 'Gestion des articles'),
+  (16, 'accueil', 'EN', 'Home'),
+  (17, 'titrePage', 'EN', 'Article Management'),
+  (28, 'Afficher', 'EN', 'Display'),
+  (29, 'Modifier', 'EN', 'Update'),
+  (30, 'Supprimer', 'EN', 'Remove'),
+  (31, 'Afficher', 'FR', 'Afficher'),
+  (32, 'Modifier', 'FR', 'Modifier'),
+  (33, 'Supprimer', 'FR', 'Supprimer'),
+  (34, 'art1', 'FR', 'c\'est un article '),
+  (35, 'art1', 'EN', 'this is an article');
